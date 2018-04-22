fb7e6e583676f39384c95d42ba31b060f2af3d25
Dmitry Volk <dvolk@gojuno.com>
2018-04-22 19:41:51 +0300

    Initial commit

---
+++ TestingPresentableComponents/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestingPresentableComponents
+//
+//  Created by Dmitry Volk on 4/22/18.
+//  Copyright © 2018 Juno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
