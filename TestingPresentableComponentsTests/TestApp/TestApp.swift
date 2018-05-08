b26e572f5a1795196807c2c27faf8b2e16562ebe
Dmitry Volk <dvolk@gojuno.com>
2018-05-08 21:40:34 +0300

    Adds TestApp

---
+++ TestingPresentableComponentsTests/TestApp/TestApp.swift
@@ -0,0 +1,37 @@
+//
+//  TestApp.swift
+//  TestingPresentableComponentsTests
+//
+//  Copyright © 2018 Juno. All rights reserved.
+//
+
+import Foundation
+
+@testable import TestingPresentableComponents
+
+final class TestApp {
+
+    let view: WelcomeScreen.TestView
+
+    init() {
+        // Inject your mock dependencies here
+        self.root = WelcomeScreen()
+        self.view = WelcomeScreen.TestView(AnyPresentable(self.root))
+    }
+
+    private let root: WelcomeScreen
+}
+
+extension TestApp {
+
+    func openSignUp() {
+        self.view.signUpAction.simpleAction()
+    }
+
+    func signUp() {
+        self.openSignUp()
+        self.view.signUpScreen.email.sink("some@email.com")
+        self.view.signUpScreen.passwordSink("123456")
+        self.view.signUpAction.simpleAction()
+    }
+}
